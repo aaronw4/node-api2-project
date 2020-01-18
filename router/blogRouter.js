977760d68d30bd7d6d0dad6386180f5f0e117735
Aaron Williams <aaronrw4@yahoo.com>
2020-01-17 19:00:50 -0800

    delete post works

--- router/blogRouter.js
+++ router/blogRouter.js
@@ -10,9 +10,65 @@ router.get('/', (req, res) => {
         })
         .catch(error => {
             res.status(500).json({
-                message: 'Error'
+                error: "The posts information could not be retrieved."
             })
         });
 });
+
+router.get('/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "The post information could not be retrieved."
+            })
+        });
+});
+
+router.post('/', (req, res) => {
+    const {title, contents} = req.body;
+
+    if (!title || !contents) {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        })
+    }
+
+    db.insert(req.body)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "There was an error while saving the post to the database"
+            })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    db.remove(req.params.id)
+        .then(deleteUser => {
+            if (deleteUser) {
+                res.status(204).end()
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist." 
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "The post could not be removed"
+            })
+        })
+})
 
 module.exports = router

6443d06e7c1e91e0abcf0028a9d2fff65f2442f6
Aaron Williams <aaronrw4@yahoo.com>
2020-01-17 22:18:21 -0800

    get comments works

--- router/blogRouter.js
+++ router/blogRouter.js
@@ -129,4 +129,24 @@ router.post('/:id/comments', (req, res) => {
         }) 
     })
 
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+
+    db.findPostComments(id)
+        .then(comment => {
+            if (comment) {
+                res.status(200).json(comment)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }            
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "The comments information could not be retrieved."
+            })
+        })
+})
+
 module.exports = router
